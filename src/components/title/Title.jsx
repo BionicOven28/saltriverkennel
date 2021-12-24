422c6ed226ad1e38c1d98c766c79a5779fd3b896
Mariah Galloway, "Reggie <71336466+BionicOven28@users.noreply.github.com>
2021-12-24 00:51:50 -0500

    title, navigation, 3 routed pages with content displaying correctly

---
+++ src/components/title/Title.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './title.css';
+
+class Title extends React.Component {
+    render() {
+        return (
+            <div className="main-title-bg">
+                <h1 id="h1a">Salt River Kennel</h1>
+            </div>
+        )
+    }
+};
+
+export default Title;
