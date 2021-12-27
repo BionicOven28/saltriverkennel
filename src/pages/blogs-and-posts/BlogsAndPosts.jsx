e7721d77b8a47e2c23e537d543bac225e91f3ad8
Mariah Galloway, "Reggie <71336466+BionicOven28@users.noreply.github.com>
2021-12-27 00:40:00 -0500

    Added a blog page
    added a blog and social media post compilation page and made sure every button still functions correctly

---
+++ src/pages/blogs-and-posts/BlogsAndPosts.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './blogs-and-posts.css';
+
+export default function BlogsAndPosts() {
+    return(
+        <>
+        <div className="hp-title">
+            <h1>Kennel Blog and Social Media Posts</h1>
+        </div>
+        <div className="hp-desc">
+            <p>a feed of just our social media posts provided by an API of some kind</p>
+            <p>blog posts about training, up to date info on the dogs and competitions, etc.</p>
+        </div>
+        </>
+    );
+}
