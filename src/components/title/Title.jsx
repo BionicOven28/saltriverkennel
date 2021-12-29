7380d80838fa8daecce295c1716530d8832920d5
Mariah Galloway, "Reggie <71336466+BionicOven28@users.noreply.github.com>
2021-12-29 00:43:47 -0500

    Started working on the CSS of the site
    worked mostly on the title, added a footer for later links

--- src/components/title/Title.jsx
+++ src/components/title/Title.jsx
@@ -6,7 +6,9 @@ class Title extends React.Component {
     render() {
         return (
             <div className="main-title-bg">
-                <h1 id="h1a">Salt River Kennel</h1>
+                <div className="main-title-box">
+                    <h1 className="main-title">Salt River Kennel</h1>
+                </div>
             </div>
         )
     }
