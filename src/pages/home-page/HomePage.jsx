fcb2b66cbbc28eec3207d0d49bde9394d009e5ca
Mariah Galloway, "Reggie <71336466+BionicOven28@users.noreply.github.com>
2022-03-01 00:29:53 -0500

    CSS Work 7
    Partially finished the front page

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -4,11 +4,18 @@ import './home-page.css';
 export default function HomePage() {
     return(
         <>
+        <div className="hp-box1">
+            <h1 className="hp-header1">Welcome</h1>
+            <p className="hp-desc1">Thank you for giving us a visit! We are Salt River Kennel -- home of the squirrel hunting Golden Retrievers!<br/> Feel free to peruse our website to learn more about what we do and why we do it.</p>
+        </div>
         <div className="hp-title">
-            <h1>Our Mission Statement</h1>
+            <h1 className="hp-title-text">Our Mission Statement</h1>
         </div>
         <div className="hp-desc">
-            <p>Here's a brief description of our kennel mission statement uwu</p>
+            <p className="hp-desc-text">Here's a brief description of our kennel mission statement uwu</p>
+        </div>
+        <div>
+            Here's an object
         </div>
         </>
     );
