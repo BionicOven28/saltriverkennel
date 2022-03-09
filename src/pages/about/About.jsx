43533a1cc0a97e8cc429345b7e3be24a03988c32
Mariah Galloway, "Reggie <71336466+BionicOven28@users.noreply.github.com>
2022-03-08 22:49:37 -0500

    misc code edits

---
+++ src/pages/about/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './about.css';
+
+export default function About() {
+    return(
+        <>
+        <div className="hp-title">
+            <h1>About</h1>
+        </div>
+        <div className="hp-desc">
+            <p>here i want to talk about golden retrievers as a breed maybe</p>
+        </div>
+        </>
+    );
+}
